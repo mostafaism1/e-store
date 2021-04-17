f3a2c4754b1eeda77470402618e95aeecf64dcf8
mostafa <me.cssign@gmail.com>
2021-04-17 20:51:04 +0200

    add fields to Order entity

--- backend/src/main/java/com/commerce/model/entity/Order.java
+++ backend/src/main/java/com/commerce/model/entity/Order.java
@@ -1,8 +1,54 @@
 package com.commerce.model.entity;
 
+import java.time.Instant;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+import com.commerce.model.common.Address;
+import com.commerce.model.common.BaseEntity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class Order {
+@Table(name = "orders")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class Order extends BaseEntity {
+
+	@ManyToOne(optional = false)
+	private User user;
+
+	@OneToMany(mappedBy = "order")
+	private List<OrderItem> orderItems;
+
+	@OneToOne
+	private Discount discount;
+
+	@ManyToOne(optional = false)
+	private Address shippingAddress;
+
+	@ManyToOne(optional = false)
+	private Address billingAddress;
+
+	private Instant placedAt;
+
+	private String shippingCompany;
+
+	private Boolean isDelivered;
+
+	@PrePersist
+	private void prePersist() {
+		placedAt = Instant.now();
+		isDelivered = false;
+	}
 
 }
