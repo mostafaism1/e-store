ab28e295050ac37b6240e7fa4d0505b094e162c4
mostafa <me.cssign@gmail.com>
2021-04-17 20:50:49 +0200

    add fields to CartItem Entity

--- backend/src/main/java/com/commerce/model/entity/CartItem.java
+++ backend/src/main/java/com/commerce/model/entity/CartItem.java
@@ -1,5 +1,26 @@
 package com.commerce.model.entity;
 
-public class CartItem {
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import com.commerce.model.common.BaseEntity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class CartItem extends BaseEntity {
+
+	@ManyToOne(optional = false)
+	private Cart cart;
+
+	@ManyToOne
+	private ProductVariant productVariant;
+
+	private Integer quantity;
 
 }
