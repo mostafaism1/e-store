d1655937fce75f5410927466c967e58796af3475
mostafa <me.cssign@gmail.com>
2021-04-17 20:47:15 +0200

    add fields to User entity

--- backend/src/main/java/com/commerce/model/entity/User.java
+++ backend/src/main/java/com/commerce/model/entity/User.java
@@ -1,8 +1,53 @@
 package com.commerce.model.entity;
 
+import java.time.Instant;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.commerce.model.common.Address;
+import com.commerce.model.common.BaseEntity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class User {
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class User extends BaseEntity {
+
+	@Email(message = "Please enter a valid email")
+	private String email;
+
+	@Size(min = 6, message = "Password must be atleast 6 characters long")
+	private String Password;
+
+	@NotBlank(message = "First name is required")
+	private String firstName;
+
+	@NotBlank(message = "Last name is required")
+	private String lastName;
+
+	@OneToOne
+	private Address address;
+
+	private Instant registeredAt;
+
+	private Boolean isVerified;
+
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+	private Cart cart;
+
+	@PrePersist
+	private void prePersist() {
+		registeredAt = Instant.now();
+	}
 
 }
