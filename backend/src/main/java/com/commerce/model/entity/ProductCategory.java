a3432ded879e39cd0a920ee89f69524fb0751489
mostafa <me.cssign@gmail.com>
2021-04-17 20:48:36 +0200

    add fields to ProductCategory entity

--- backend/src/main/java/com/commerce/model/entity/ProductCategory.java
+++ backend/src/main/java/com/commerce/model/entity/ProductCategory.java
@@ -1,8 +1,21 @@
 package com.commerce.model.entity;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import com.commerce.model.common.BaseEntity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class ProductCategory {
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class ProductCategory extends BaseEntity {
+
+	@NotBlank(message = "Category name is required")
+	private String name;
 
 }
