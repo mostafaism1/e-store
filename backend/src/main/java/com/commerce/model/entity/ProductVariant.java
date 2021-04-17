113db665cc1361ab638188ce222dfb3f953c8eac
mostafa <me.cssign@gmail.com>
2021-04-17 20:49:05 +0200

    add fields to ProductVariant entity

--- backend/src/main/java/com/commerce/model/entity/ProductVariant.java
+++ backend/src/main/java/com/commerce/model/entity/ProductVariant.java
@@ -1,8 +1,51 @@
 package com.commerce.model.entity;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import com.commerce.model.common.BaseEntity;
+import com.commerce.model.common.ShippingInformation;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class ProductVariant {
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class ProductVariant extends BaseEntity {
+
+	@ManyToOne(optional = false)
+	@NotNull(message = "Product is required")
+	private Product product;
+
+	@ManyToOne
+	private Color color;
+
+	@Embedded
+	private ShippingInformation shippingInformation;
+
+	private String composition;
+	
+	@Min(value = 0, message = "Price must be a positive number")
+	private Double price;
+
+	@Min(value = 0, message = "Tax percent must be a positive number")
+	private Double taxPercent;
+
+	private String imageURL;
+
+	private String thumbURL;
+
+	@Min(value = 0, message = "Stock must be a positive number")
+	private Integer stock;
+
+	private Integer sellCount;
+	
+	private Boolean isActive;
 
 }
