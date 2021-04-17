4baa65ed35cafd98e616cb921357c0c56c1de2ca
mostafa <me.cssign@gmail.com>
2021-04-17 20:51:19 +0200

    add fields to OrderItem entity

--- backend/src/main/java/com/commerce/model/entity/OrderItem.java
+++ backend/src/main/java/com/commerce/model/entity/OrderItem.java
@@ -1,8 +1,26 @@
 package com.commerce.model.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import com.commerce.model.common.BaseEntity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class OrderItem {
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class OrderItem extends BaseEntity {
+
+	@ManyToOne(optional = false)
+	private Order order;
+
+	@ManyToOne(optional = false)
+	private ProductVariant productVariant;
+
+	private Integer quantity;
 
 }
