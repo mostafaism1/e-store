0f246926fd030c5a635d60ae95401921b609e1ec
mostafa <me.cssign@gmail.com>
2021-04-17 20:48:04 +0200

    add fields to PasswordForgotToken entity

--- backend/src/main/java/com/commerce/model/entity/PasswordForgotToken.java
+++ backend/src/main/java/com/commerce/model/entity/PasswordForgotToken.java
@@ -1,8 +1,22 @@
 package com.commerce.model.entity;
 
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 
+import com.commerce.model.common.BaseEntity;
+import com.commerce.model.common.Token;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 @Entity
-public class PasswordForgotToken {
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class PasswordForgotToken extends BaseEntity {
+
+	@Embedded
+	private Token token;
 
 }
