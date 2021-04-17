eb6c6d65f250f4fff0758261e8c34d6ce9708a66
mostafa <me.cssign@gmail.com>
2021-04-17 20:49:22 +0200

    add fields to Color entity

--- backend/src/main/java/com/commerce/model/entity/Color.java
+++ backend/src/main/java/com/commerce/model/entity/Color.java
@@ -1,8 +1,31 @@
 package com.commerce.model.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+
+import com.commerce.model.common.BaseEntity;
+import com.commerce.validation.HexColor;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Entity
-public class Color {
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class Color extends BaseEntity {
+
+	@OneToMany(mappedBy = "color")
+	private List<ProductVariant> productVariants;
+
+	@NotBlank(message = "Color name is required")
+	private String name;
+
+	@HexColor
+	private String hex;
 
 }
