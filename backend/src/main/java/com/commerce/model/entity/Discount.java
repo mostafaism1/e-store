5d35d7721cefb592993e1fc1e3c7c6e418dcb1c3
mostafa <me.cssign@gmail.com>
2021-04-17 20:50:22 +0200

    add fields to Discount entity

--- backend/src/main/java/com/commerce/model/entity/Discount.java
+++ backend/src/main/java/com/commerce/model/entity/Discount.java
@@ -1,5 +1,31 @@
 package com.commerce.model.entity;
 
-public class Discount {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.commerce.model.common.BaseEntity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public class Discount extends BaseEntity {
+
+	@OneToMany(mappedBy = "discount")
+	private List<Order> orders;
+
+	@OneToMany(mappedBy = "discount")
+	private List<Cart> carts;
+
+	private String code;
+
+	private Integer discountPercent;
+
+	private Boolean isActive;
 }
