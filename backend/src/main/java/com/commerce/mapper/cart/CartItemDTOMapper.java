a44c1506c8379f96463a08dd0d068bdcece7ce0f
mostafa <me.cssign@gmail.com>
2021-04-19 00:59:58 +0200

    create mappers for Cart related entities

---
+++ backend/src/main/java/com/commerce/mapper/cart/CartItemDTOMapper.java
@@ -0,0 +1,30 @@
+package com.commerce.mapper.cart;
+
+import java.util.function.Function;
+
+import com.commerce.mapper.color.ColorDTOMapper;
+import com.commerce.model.dto.CartItemDTO;
+import com.commerce.model.entity.CartItem;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class CartItemDTOMapper implements Function<CartItem, CartItemDTO> {
+
+    private final ColorDTOMapper colorDTOMapper;
+
+    @Override
+    public CartItemDTO apply(CartItem cartItem) {
+
+        return CartItemDTO.builder().id(cartItem.getId()).url(cartItem.getProductVariant().getProduct().getUrl())
+                .name(cartItem.getProductVariant().getProduct().getName())
+                .price(cartItem.getProductVariant().getPrice()).quantity(cartItem.getQuantity())
+                .thumbURL(cartItem.getProductVariant().getThumbURL()).stock(cartItem.getProductVariant().getStock())
+                .color(colorDTOMapper.apply(cartItem.getProductVariant().getColor())).build();
+
+    }
+
+}
