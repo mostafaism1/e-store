5ee0db2d49a16ff633935a8dc9b19bf235e2a772
mostafa <me.cssign@gmail.com>
2021-04-18 05:05:32 +0200

    create model for Cart response

---
+++ backend/src/main/java/com/commerce/model/response/cart/CartResponse.java
@@ -0,0 +1,19 @@
+package com.commerce.model.response.cart;
+
+import java.util.List;
+
+import com.commerce.model.dto.CartItemDTO;
+import com.commerce.model.dto.DiscountDTO;
+
+import lombok.Data;
+
+@Data
+public class CartResponse {
+
+    private List<CartItemDTO> cartItems;
+    private DiscountDTO discount;
+    private Double totalCartPrice;
+    private Double totalCargoPrice;
+    private Double totalPrice;
+
+}
