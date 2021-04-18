f9c289e22dd315aff797f2925dc0450caf0cf7f8
mostafa <me.cssign@gmail.com>
2021-04-19 00:57:40 +0200

    create mapper for User

---
+++ backend/src/main/java/com/commerce/mapper/user/UserResponseMapper.java
@@ -0,0 +1,34 @@
+package com.commerce.mapper.user;
+
+import java.util.function.Function;
+
+import com.commerce.mapper.common.AddressDTOMapper;
+import com.commerce.model.entity.User;
+import com.commerce.model.response.user.UserResponse;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+
+@Component
+@AllArgsConstructor
+public class UserResponseMapper implements Function<User, UserResponse> {
+
+    private AddressDTOMapper addressDTOMapper;
+
+    @Override
+    public UserResponse apply(User user) {
+
+        UserResponse userResponse = new UserResponse();
+
+        userResponse.setEmail(user.getEmail());
+        userResponse.setFirstName(user.getFirstName());
+        userResponse.setLastName(user.getLastName());
+        userResponse.setAddress(addressDTOMapper.apply(user.getAddress()));
+        userResponse.setIsVerified(user.getIsVerified());
+
+        return userResponse;
+
+    }
+
+}
