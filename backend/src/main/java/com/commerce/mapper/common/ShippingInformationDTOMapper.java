d04258e1c06d93b6587c290d89a5f305fb75676b
mostafa <me.cssign@gmail.com>
2021-04-19 00:58:48 +0200

    create mapper for ShippingInformation

---
+++ backend/src/main/java/com/commerce/mapper/common/ShippingInformationDTOMapper.java
@@ -0,0 +1,22 @@
+package com.commerce.mapper.common;
+
+import java.util.function.Function;
+
+import com.commerce.model.common.ShippingInformation;
+import com.commerce.model.dto.ShippingInformationDTO;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ShippingInformationDTOMapper implements Function<ShippingInformation, ShippingInformationDTO> {
+
+    @Override
+    public ShippingInformationDTO apply(ShippingInformation shippingInformation) {
+
+        return ShippingInformationDTO.builder().length(shippingInformation.getLength())
+                .width(shippingInformation.getWidth()).height(shippingInformation.getHeight())
+                .weight(shippingInformation.getWeight()).build();
+
+    }
+
+}
