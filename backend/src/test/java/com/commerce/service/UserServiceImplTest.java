d7ca17069a63a76cc42c619114a8a6c7f95a8ad9
mostafa <me.cssign@gmail.com>
2021-04-20 22:29:32 +0200

    refactor test to use BDD style verification

--- backend/src/test/java/com/commerce/service/UserServiceImplTest.java
+++ backend/src/test/java/com/commerce/service/UserServiceImplTest.java
@@ -1,12 +1,16 @@
 package com.commerce.service;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.commerce.dao.UserRepository;
+import com.commerce.error.exception.InvalidArgumentException;
 import com.commerce.model.entity.User;
 import com.commerce.model.request.user.RegisterRequest;
 import com.commerce.model.response.user.UserResponse;
@@ -15,6 +19,7 @@ import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -39,7 +44,7 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void Should_Register_Given_NonConflictingEmailAddress() {
+    void Should_Register_Given_NonConflictingEmailAddress() {
 
         // given
         String email = faker.internet().emailAddress();
@@ -67,8 +72,8 @@ public class UserServiceImplTest {
         // when
         UserResponse userActual = userService.register(registerUserRequest);
 
-        // then
-        verify(userRepository).save(any(User.class));
+        // then        
+        BDDMockito.then(userRepository).should(times(1)).save(any(User.class));
         then(userActual).usingRecursiveComparison().isEqualTo(userExpected);
 
     }
