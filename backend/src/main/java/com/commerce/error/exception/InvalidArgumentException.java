ad5cbf3c58a162691d4b781d80eb3e1eb42a64f7
mostafa <me.cssign@gmail.com>
2021-04-20 22:31:05 +0200

    create an InvalidArgumentException class

---
+++ backend/src/main/java/com/commerce/error/exception/InvalidArgumentException.java
@@ -0,0 +1,9 @@
+package com.commerce.error.exception;
+
+public class InvalidArgumentException extends RuntimeException {
+
+    public InvalidArgumentException(String message) {
+        super(message);
+    }
+
+}
