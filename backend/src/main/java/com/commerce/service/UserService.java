5a03b86a0ab310ba65e7d67df378d57ebfc26aa2
mostafa <me.cssign@gmail.com>
2021-04-20 20:52:54 +0200

    define the user service interface

---
+++ backend/src/main/java/com/commerce/service/UserService.java
@@ -0,0 +1,30 @@
+package com.commerce.service;
+
+import com.commerce.model.entity.User;
+import com.commerce.model.request.user.PasswordResetRequest;
+import com.commerce.model.request.user.RegisterRequest;
+import com.commerce.model.request.user.UpdateUserAddressRequest;
+import com.commerce.model.request.user.UpdateUserRequest;
+import com.commerce.model.response.user.UserResponse;
+
+public interface UserService {
+
+    UserResponse getCurrentUser();
+
+    UserResponse saveUser(User user);
+
+    UserResponse findByEmail(String email);
+
+    UserResponse register(RegisterRequest registerRequest);
+
+    UserResponse updateUser(UpdateUserRequest updateUserRequest);
+
+    UserResponse updateUserAddress(UpdateUserAddressRequest updateUserAddressRequest);
+
+    void resetPassword(PasswordResetRequest passwordResetRequest);
+
+    Boolean userExists(String email);
+
+    Boolean getVerificationStatus();
+
+}
