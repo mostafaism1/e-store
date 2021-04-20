dee7e2867941b3abfc27cd6bcbc26b4d89b3e6e2
mostafa <me.cssign@gmail.com>
2021-04-20 21:36:43 +0200

    test successful user registration

---
+++ backend/src/test/java/com/commerce/service/UserServiceImplTest.java
@@ -0,0 +1,76 @@
+package com.commerce.service;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.commerce.dao.UserRepository;
+import com.commerce.model.entity.User;
+import com.commerce.model.request.user.RegisterRequest;
+import com.commerce.model.response.user.UserResponse;
+import com.github.javafaker.Faker;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class UserServiceImplTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    private Faker faker;
+
+    @BeforeEach
+    public void setUp() {
+        faker = new Faker();
+    }
+
+    @Test
+    public void shouldRegisterUser() {
+
+        // given
+        String email = faker.internet().emailAddress();
+        String password = faker.internet().password(6, 52);
+        RegisterRequest registerUserRequest = new RegisterRequest();
+        registerUserRequest.setEmail(email);
+        registerUserRequest.setPassword(password);
+        registerUserRequest.setPasswordConfirmation(password);
+
+        UserResponse userExpected = new UserResponse();
+        userExpected.setEmail(email);
+        userExpected.setAddress(null);
+        userExpected.setFirstName(null);
+        userExpected.setLastName(null);
+        userExpected.setIsVerified(false);
+
+        given(userRepository.existsByEmail(email)).willReturn(false);
+
+        given(userRepository.save(any(User.class))).willAnswer(invocation -> {
+            User user = (User) invocation.getArguments()[0];
+            ReflectionTestUtils.setField(user, "id", 123L);
+            return user;
+        });
+
+        // when
+        UserResponse userActual = userService.register(registerUserRequest);
+
+        // then
+        verify(userRepository).save(any(User.class));
+        then(userActual).usingRecursiveComparison().isEqualTo(userExpected);
+
+    }
+
+}
