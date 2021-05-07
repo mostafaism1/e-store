dcc299e6b1f50e6a802cd4cc51e20eaf23fb2b9a
mostafa <me.cssign@gmail.com>
2021-05-07 08:08:51 +0200

    add tests for TokenService.createPasswordResetToken()

--- backend/src/test/java/com/commerce/service/token/TokenServiceImplTest.java
+++ backend/src/test/java/com/commerce/service/token/TokenServiceImplTest.java
@@ -5,10 +5,18 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
+import java.util.Optional;
+
+import com.commerce.dao.PasswordForgotTokenRepository;
+import com.commerce.dao.UserRepository;
 import com.commerce.dao.VerificationTokenRepository;
+import com.commerce.model.entity.PasswordForgotToken;
 import com.commerce.model.entity.User;
 import com.commerce.model.entity.VerificationToken;
+import com.commerce.model.event.OnPasswordForgotRequestEvent;
 import com.commerce.model.event.OnRegistrationCompleteEvent;
+import com.commerce.service.user.UserService;
+import com.github.javafaker.Faker;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,47 +26,120 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 class TokenServiceImplTest {
 
-    @InjectMocks
-    private TokenServiceImpl tokenService;
+        @InjectMocks
+        private TokenServiceImpl tokenService;
 
-    @Mock
-    private ApplicationEventPublisher eventPublisher;
+        @Mock
+        private UserService userService;
 
-    @Mock
-    private VerificationTokenRepository verificationTokenRepository;
+        @Mock
+        private PasswordEncoder passwordEncoder;
 
-    private User user;
+        @Mock
+        private ApplicationEventPublisher eventPublisher;
 
-    @BeforeEach
-    public void setUp() {
-        user = new User();
-    }
+        @Mock
+        private VerificationTokenRepository verificationTokenRepository;
 
-    @Test
-    void it_should_create_email_confirm_token() {
+        @Mock
+        private PasswordForgotTokenRepository passwordForgotTokenRepository;
 
-        // given
-        ArgumentCaptor<VerificationToken> verificationTokenArgumentCaptor = ArgumentCaptor
-                .forClass(VerificationToken.class);
-        ArgumentCaptor<OnRegistrationCompleteEvent> onRegistrationCompleteEventArgumentCaptor = ArgumentCaptor
-                .forClass(OnRegistrationCompleteEvent.class);
+        @Mock
+        private UserRepository userRepository;
 
-        given(verificationTokenRepository.save(any(VerificationToken.class))).willReturn(new VerificationToken());
+        private User user;
 
-        // when
-        tokenService.createEmailConfirmToken(user);
+        private Faker faker;
 
-        // then
-        verify(verificationTokenRepository).save(verificationTokenArgumentCaptor.capture());
-        verify(eventPublisher).publishEvent(onRegistrationCompleteEventArgumentCaptor.capture());
+        @BeforeEach
+        public void setUp() {
+                user = new User();
+                faker = new Faker();
+        }
 
-        then(user).isEqualTo(onRegistrationCompleteEventArgumentCaptor.getValue().getUser());
-        then(verificationTokenArgumentCaptor.getValue().getToken().getToken())
-                .isEqualTo(onRegistrationCompleteEventArgumentCaptor.getValue().getToken());
+        @Test
+        void it_should_create_email_confirm_token() {
 
-    }
+                // given
+                ArgumentCaptor<VerificationToken> verificationTokenArgumentCaptor = ArgumentCaptor
+                                .forClass(VerificationToken.class);
+                ArgumentCaptor<OnRegistrationCompleteEvent> onRegistrationCompleteEventArgumentCaptor = ArgumentCaptor
+                                .forClass(OnRegistrationCompleteEvent.class);
+
+                given(verificationTokenRepository.save(any(VerificationToken.class))).willReturn(new VerificationToken());
+
+                // when
+                tokenService.createEmailConfirmToken(user);
+
+                // then
+                verify(verificationTokenRepository).save(verificationTokenArgumentCaptor.capture());
+                verify(eventPublisher).publishEvent(onRegistrationCompleteEventArgumentCaptor.capture());
+
+                then(user).isEqualTo(onRegistrationCompleteEventArgumentCaptor.getValue().getUser());
+                then(verificationTokenArgumentCaptor.getValue().getToken().getToken())
+                                .isEqualTo(onRegistrationCompleteEventArgumentCaptor.getValue().getToken());
+
+        }
+
+        @Test
+        void it_should_create_password_reset_token_if_it_does_not_exist() {
+
+                // given
+                String email = faker.internet().emailAddress();
+                ArgumentCaptor<PasswordForgotToken> passwordForgotTokenArgumentCaptor = ArgumentCaptor
+                                .forClass(PasswordForgotToken.class);
+                ArgumentCaptor<OnPasswordForgotRequestEvent> onPasswordForgotRequestEventArgumentCaptor = ArgumentCaptor
+                                .forClass(OnPasswordForgotRequestEvent.class);
+
+                given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
+                given(passwordForgotTokenRepository.findByTokenUser(user)).willReturn(Optional.empty());
+                given(passwordForgotTokenRepository.save(any(PasswordForgotToken.class)))
+                                .willReturn(new PasswordForgotToken());
+
+                // when
+                tokenService.createPasswordResetToken(email);
+
+                // then
+                verify(passwordForgotTokenRepository).save(passwordForgotTokenArgumentCaptor.capture());
+                verify(eventPublisher).publishEvent(onPasswordForgotRequestEventArgumentCaptor.capture());
+
+                then(user).isEqualTo(onPasswordForgotRequestEventArgumentCaptor.getValue().getUser());
+                then(passwordForgotTokenArgumentCaptor.getValue().getToken().getToken())
+                                .isEqualTo(onPasswordForgotRequestEventArgumentCaptor.getValue().getToken());
+
+        }
+
+        @Test
+        void it_should_create_password_reset_token_if_it_already_exists() {
+
+                // given
+                String email = faker.internet().emailAddress();
+                ArgumentCaptor<PasswordForgotToken> passwordForgotTokenArgumentCaptor = ArgumentCaptor
+                                .forClass(PasswordForgotToken.class);
+                ArgumentCaptor<OnPasswordForgotRequestEvent> onPasswordForgotRequestEventArgumentCaptor = ArgumentCaptor
+                                .forClass(OnPasswordForgotRequestEvent.class);
+
+                given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
+                given(passwordForgotTokenRepository.findByTokenUser(user))
+                                .willReturn(Optional.of(new PasswordForgotToken()));
+                given(passwordForgotTokenRepository.save(any(PasswordForgotToken.class)))
+                                .willReturn(new PasswordForgotToken());
+
+                // when
+                tokenService.createPasswordResetToken(email);
+
+                // then
+                verify(passwordForgotTokenRepository).save(passwordForgotTokenArgumentCaptor.capture());
+                verify(eventPublisher).publishEvent(onPasswordForgotRequestEventArgumentCaptor.capture());
+
+                then(user).isEqualTo(onPasswordForgotRequestEventArgumentCaptor.getValue().getUser());
+                then(passwordForgotTokenArgumentCaptor.getValue().getToken().getToken())
+                                .isEqualTo(onPasswordForgotRequestEventArgumentCaptor.getValue().getToken());
+
+        }
 }
