a70aee1292d1000b9b916c6a35c0bd99938d3721
mostafa <me.cssign@gmail.com>
2021-05-08 04:48:06 +0200

    create an empty ProductCategoryService implementation

---
+++ backend/src/main/java/com/commerce/service/productCategory/ProductCategoryServiceImpl.java
@@ -0,0 +1,15 @@
+package com.commerce.service.productCategory;
+
+import java.util.List;
+
+import com.commerce.model.response.category.ProductCategoryResponse;
+
+public class ProductCategoryServiceImpl implements ProductCategoryService {
+
+    @Override
+    public List<ProductCategoryResponse> findAllByOrderByName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
