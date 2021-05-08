7013671f90ed07cc6b7d1e204f025b1107ed076e
mostafa <me.cssign@gmail.com>
2021-05-08 23:29:26 +0200

    create an empty ProductColorService implementation

---
+++ backend/src/main/java/com/commerce/service/color/ProductColorServiceImpl.java
@@ -0,0 +1,15 @@
+package com.commerce.service.color;
+
+import java.util.List;
+
+import com.commerce.model.response.color.ProductColorResponse;
+
+public class ProductColorServiceImpl implements ProductColorService {
+
+    @Override
+    public List<ProductColorResponse> findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
