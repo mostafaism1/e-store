0712e8b48f3eecc5a97a06bc4b6d18f74ccca873
mostafa <me.cssign@gmail.com>
2021-05-08 23:28:50 +0200

    define ProductColorService interface

---
+++ backend/src/main/java/com/commerce/service/color/ProductColorService.java
@@ -0,0 +1,11 @@
+package com.commerce.service.color;
+
+import java.util.List;
+
+import com.commerce.model.response.color.ProductColorResponse;
+
+public interface ProductColorService {
+
+    List<ProductColorResponse> findAll();
+
+}
