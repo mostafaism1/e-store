8475a3bf5bf89f487cc7b9dfd484dfec7a392232
mostafa <me.cssign@gmail.com>
2021-05-09 00:49:41 +0200

    create an empty DiscountService implementation

---
+++ backend/src/main/java/com/commerce/service/discount/DiscountServiceImpl.java
@@ -0,0 +1,11 @@
+package com.commerce.service.discount;
+
+public class DiscountServiceImpl implements DiscountService {
+
+    @Override
+    public void applyDiscount(String code) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
