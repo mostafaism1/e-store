43db70c55bfd055f508b4688611ec01de792d6be
mostafa <me.cssign@gmail.com>
2021-05-08 11:27:41 +0200

    create CategoryResponseMapper

---
+++ backend/src/main/java/com/commerce/mapper/category/CategoryResponseMapper.java
@@ -0,0 +1,27 @@
+package com.commerce.mapper.category;
+
+import java.util.function.Function;
+
+import com.commerce.model.dto.CategoryDTO;
+import com.commerce.model.entity.ProductCategory;
+import com.commerce.model.response.category.ProductCategoryResponse;
+
+public class CategoryResponseMapper implements Function<ProductCategory, ProductCategoryResponse> {
+
+    @Override
+    public ProductCategoryResponse apply(ProductCategory productCategory) {
+
+        if (productCategory == null) {
+            return null;
+        }
+
+        CategoryDTO categoryDTO = new CategoryDTO(productCategory.getName());
+
+        ProductCategoryResponse productCategoryResponse = new ProductCategoryResponse();
+        productCategoryResponse.setCategory(categoryDTO);
+
+        return productCategoryResponse;
+
+    }
+
+}
