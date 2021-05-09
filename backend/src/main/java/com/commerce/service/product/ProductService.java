2b51f629fa396c0fff7d71ce9b0a9237761bbfb6
mostafa <me.cssign@gmail.com>
2021-05-09 03:47:49 +0200

    define ProductService interface

---
+++ backend/src/main/java/com/commerce/service/product/ProductService.java
@@ -0,0 +1,30 @@
+package com.commerce.service.product;
+
+import java.util.List;
+
+import com.commerce.model.response.product.ProductDetailsResponse;
+import com.commerce.model.response.product.ProductResponse;
+import com.commerce.model.response.product.ProductVariantResponse;
+
+public interface ProductService {
+
+    ProductDetailsResponse findByUrl(String url);
+
+    List<ProductVariantResponse> getAll(Integer page, Integer size, String sort, String category, Double minPrice,
+            Double maxPrice, String color);
+
+    Long getAllCount(String category, Double minPrice, Double maxPrice, String color);
+
+    ProductVariantResponse findProductVariantById(Long id);
+
+    List<ProductResponse> getRelatedProducts(String url);
+
+    List<ProductResponse> getNewlyAddedProducts();
+
+    List<ProductVariantResponse> getMostSelling();
+
+    List<ProductResponse> getInterested();
+
+    List<ProductResponse> searchProductDisplay(String keyword, Integer page, Integer size);
+
+}
