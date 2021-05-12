30cb786e7a2719a9b97e85252e07815f9b3667fb
mostafa <me.cssign@gmail.com>
2021-05-12 11:22:59 +0200

    create ResourceFetchException class

---
+++ backend/src/main/java/com/commerce/error/exception/ResourceFetchException.java
@@ -0,0 +1,9 @@
+package com.commerce.error.exception;
+
+public class ResourceFetchException extends RuntimeException {
+
+    public ResourceFetchException(String message) {
+        super(message);
+    }
+
+}
