3fe1171b0b7507244be0b89d194a4f9204f4b784
mostafa <me.cssign@gmail.com>
2021-05-12 11:23:26 +0200

    implement OrderService.getAllOrdersCount()

--- backend/src/main/java/com/commerce/service/order/OrderServiceImpl.java
+++ backend/src/main/java/com/commerce/service/order/OrderServiceImpl.java
@@ -2,15 +2,35 @@ package com.commerce.service.order;
 
 import java.util.List;
 
+import com.commerce.dao.OrderRepository;
+import com.commerce.dao.UserRepository;
+import com.commerce.error.exception.ResourceFetchException;
+import com.commerce.model.entity.User;
 import com.commerce.model.request.order.PostOrderRequest;
 import com.commerce.model.response.order.OrderResponse;
+import com.commerce.model.response.user.UserResponse;
+import com.commerce.service.user.UserService;
 
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class OrderServiceImpl implements OrderService {
+
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
 
     @Override
     public Integer getAllOrdersCount() {
-        // TODO Auto-generated method stub
-        return null;
+
+        User user = getCurrentUser();
+
+        return orderRepository.countAllByUser(user)
+                .orElseThrow(() -> new ResourceFetchException("An error occurred whilst fetching orders count"));
+
     }
 
     @Override
@@ -23,6 +43,12 @@ public class OrderServiceImpl implements OrderService {
     public OrderResponse postOrder(PostOrderRequest postOrderRequest) {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    private User getCurrentUser() {
+        UserResponse currentUser = userService.getCurrentUser();
+        User user = userRepository.findByEmail(currentUser.getEmail()).get();
+        return user;
     }
 
 }
