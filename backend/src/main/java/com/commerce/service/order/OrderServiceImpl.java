9a767be29d4e150d8109bfb3c22487975b67ac6b
mostafa <me.cssign@gmail.com>
2021-05-12 10:57:00 +0200

    create empty OrderService implementation

---
+++ backend/src/main/java/com/commerce/service/order/OrderServiceImpl.java
@@ -0,0 +1,28 @@
+package com.commerce.service.order;
+
+import java.util.List;
+
+import com.commerce.model.request.order.PostOrderRequest;
+import com.commerce.model.response.order.OrderResponse;
+
+public class OrderServiceImpl implements OrderService {
+
+    @Override
+    public Integer getAllOrdersCount() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<OrderResponse> getAllOrders(Integer page, Integer pageSize) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public OrderResponse postOrder(PostOrderRequest postOrderRequest) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
