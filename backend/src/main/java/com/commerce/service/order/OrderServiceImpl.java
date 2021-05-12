f7fcb14c054e5df02c6831fb186b64973235329b
mostafa <me.cssign@gmail.com>
2021-05-12 11:31:55 +0200

    implement OrderService.getAllOrders()

--- backend/src/main/java/com/commerce/service/order/OrderServiceImpl.java
+++ backend/src/main/java/com/commerce/service/order/OrderServiceImpl.java
@@ -1,16 +1,19 @@
 package com.commerce.service.order;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.commerce.dao.OrderRepository;
 import com.commerce.dao.UserRepository;
 import com.commerce.error.exception.ResourceFetchException;
+import com.commerce.mapper.order.OrderResponseMapper;
 import com.commerce.model.entity.User;
 import com.commerce.model.request.order.PostOrderRequest;
 import com.commerce.model.response.order.OrderResponse;
 import com.commerce.model.response.user.UserResponse;
 import com.commerce.service.user.UserService;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -22,6 +25,7 @@ public class OrderServiceImpl implements OrderService {
     private final UserService userService;
     private final UserRepository userRepository;
     private final OrderRepository orderRepository;
+    private final OrderResponseMapper orderResponseMapper;
 
     @Override
     public Integer getAllOrdersCount() {
@@ -35,8 +39,14 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public List<OrderResponse> getAllOrders(Integer page, Integer pageSize) {
-        // TODO Auto-generated method stub
-        return null;
+
+        User user = getCurrentUser();
+
+        PageRequest pageRequest = PageRequest.of(page, pageSize);
+
+        return orderRepository.findAllByUserOrderByPlacedAtDesc(user, pageRequest).stream().map(orderResponseMapper)
+                .collect(Collectors.toList());
+
     }
 
     @Override
