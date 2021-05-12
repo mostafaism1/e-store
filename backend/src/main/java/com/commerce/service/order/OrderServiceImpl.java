b3d98d00f52d7feedc5687be31a95e971027b820
mostafa <me.cssign@gmail.com>
2021-05-12 12:43:41 +0200

    implement OrderService.postOrder()

--- backend/src/main/java/com/commerce/service/order/OrderServiceImpl.java
+++ backend/src/main/java/com/commerce/service/order/OrderServiceImpl.java
@@ -1,16 +1,23 @@
 package com.commerce.service.order;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.commerce.dao.OrderRepository;
 import com.commerce.dao.UserRepository;
+import com.commerce.error.exception.InvalidArgumentException;
 import com.commerce.error.exception.ResourceFetchException;
+import com.commerce.mapper.common.AddressDTOToAddressMapper;
 import com.commerce.mapper.order.OrderResponseMapper;
+import com.commerce.model.entity.Cart;
+import com.commerce.model.entity.Order;
+import com.commerce.model.entity.OrderItem;
 import com.commerce.model.entity.User;
 import com.commerce.model.request.order.PostOrderRequest;
 import com.commerce.model.response.order.OrderResponse;
 import com.commerce.model.response.user.UserResponse;
+import com.commerce.service.cart.CartService;
 import com.commerce.service.user.UserService;
 
 import org.springframework.data.domain.PageRequest;
@@ -26,6 +33,8 @@ public class OrderServiceImpl implements OrderService {
     private final UserRepository userRepository;
     private final OrderRepository orderRepository;
     private final OrderResponseMapper orderResponseMapper;
+    private final CartService cartService;
+    private final AddressDTOToAddressMapper addressDTOToAddressMapper;
 
     @Override
     public Integer getAllOrdersCount() {
@@ -51,8 +60,54 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderResponse postOrder(PostOrderRequest postOrderRequest) {
-        // TODO Auto-generated method stub
-        return null;
+
+        User user = getCurrentUser();
+        Cart cart = user.getCart();
+        if (cart == null || cart.getCartItems() == null) {
+            throw new InvalidArgumentException("Cart is not valid");
+        }
+
+        checkStock(cart);
+
+        Order order = createOrder(postOrderRequest, user, cart);
+
+        Order savedOrder = orderRepository.save(order);
+
+        cartService.emptyCart();
+
+        return orderResponseMapper.apply(savedOrder);
+
+    }
+
+    private Order createOrder(PostOrderRequest postOrderRequest, User user, Cart cart) {
+        final Order order = new Order();
+        order.setOrderItems(new ArrayList<>());
+        cart.getCartItems().forEach(cartItem -> {
+            cartItem.getProductVariant()
+                    .setSellCount(cartItem.getProductVariant().getSellCount() + cartItem.getQuantity());
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProductVariant(cartItem.getProductVariant());
+            orderItem.setQuantity(cartItem.getQuantity());
+            orderItem.setOrder(order);
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.add(orderItem);
+            order.setOrderItems(orderItems);
+        });
+        order.setUser(user);
+        order.setDiscount(cart.getDiscount());
+        order.setBillingAddress(addressDTOToAddressMapper.apply(postOrderRequest.getBillingAddress()));
+        order.setShippingAddress(addressDTOToAddressMapper.apply(postOrderRequest.getShippingAddress()));
+        order.setShippingCompany(postOrderRequest.getShippingCompany());
+        return order;
+    }
+
+    private void checkStock(Cart cart) {
+        if (cart.getCartItems().stream()
+                .anyMatch(cartItem -> cartItem.getQuantity() > cartItem.getProductVariant().getStock())) {
+            throw new InvalidArgumentException("A product in your cart is out of stock.");
+        }
     }
 
     private User getCurrentUser() {
