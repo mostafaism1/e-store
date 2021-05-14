c069763e00bbad4af646bc64ee290c9e5b724b61
mostafa <me.cssign@gmail.com>
2021-05-14 15:24:25 +0200

    create SecurityConstants

---
+++ backend/src/main/java/com/commerce/constants/SecurityConstants.java
@@ -0,0 +1,36 @@
+package com.commerce.constants;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@Data
+public class SecurityConstants {
+
+    @Value("${security.public-pattern}")
+    private String publicPattern;
+
+    @Value("${security.auth.username}")
+    private String authUsername;
+
+    @Value("${security.auth.url}")
+    private String authUrl;
+
+    @Value("${security.auth.client_id}")
+    private String clientId;
+
+    @Value("${security.auth.client_password}")
+    private String clientPassword;
+
+    @Value("${security.auth.connection_timeout}")
+    private String connectionTimeout;
+
+    @Value("${security.auth.read_timeout}")
+    private String readTimeout;
+
+    @Value("${security.auth.whitelist}")
+    private String[] whitelist;
+
+}
