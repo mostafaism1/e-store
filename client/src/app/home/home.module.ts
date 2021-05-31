fc1f0e58b293743e4d48dd8d7b3cf55e0aaef8e4
mostafa <me.cssign@gmail.com>
2021-05-31 17:31:03 +0200

    generate most-selling component

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -5,13 +5,15 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { BannerComponent } from './banner/banner.component';
 import { ImageSliderComponent } from './image-slider/image-slider.component';
+import { MostSellingComponent } from './most-selling/most-selling.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     BannerComponent,
-    ImageSliderComponent
+    ImageSliderComponent,
+    MostSellingComponent
   ],
   imports: [
     CommonModule,
---
+++ client/src/app/home/most-selling/most-selling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-most-selling',
+  templateUrl: './most-selling.component.html',
+  styleUrls: ['./most-selling.component.scss']
+})
+export class MostSellingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
