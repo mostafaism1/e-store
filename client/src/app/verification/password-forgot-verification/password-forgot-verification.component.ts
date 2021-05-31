1d23326cad92f3e3c7422e82555712380aceb2ac
mostafa <me.cssign@gmail.com>
2021-05-31 16:35:20 +0200

    generate password-forgot-verification component

---
+++ client/src/app/verification/password-forgot-verification/password-forgot-verification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-forgot-verification',
+  templateUrl: './password-forgot-verification.component.html',
+  styleUrls: ['./password-forgot-verification.component.scss']
+})
+export class PasswordForgotVerificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- client/src/app/verification/verification.module.ts
+++ client/src/app/verification/verification.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { VerificationRoutingModule } from './verification-routing.module';
 import { EmailVerificationComponent } from './email-verification/email-verification.component';
+import { PasswordForgotVerificationComponent } from './password-forgot-verification/password-forgot-verification.component';
 
 
 @NgModule({
   declarations: [
-    EmailVerificationComponent
+    EmailVerificationComponent,
+    PasswordForgotVerificationComponent
   ],
   imports: [
     CommonModule,
