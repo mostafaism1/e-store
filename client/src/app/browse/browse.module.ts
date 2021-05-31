041c48834b42679a051d1bcf1820f4506ecda2cf
mostafa <me.cssign@gmail.com>
2021-05-31 17:01:22 +0200

    generate browse component

--- client/src/app/browse/browse.module.ts
+++ client/src/app/browse/browse.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BrowseRoutingModule } from './browse-routing.module';
+import { BrowseComponent } from './browse.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BrowseComponent
+  ],
   imports: [
     CommonModule,
     BrowseRoutingModule
