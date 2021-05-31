0abcc12fbc85784403504e547fe52fcf10076560
mostafa <me.cssign@gmail.com>
2021-05-31 17:10:37 +0200

    generate product-detail component

--- client/src/app/product-detail/product-detail.module.ts
+++ client/src/app/product-detail/product-detail.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductDetailRoutingModule } from './product-detail-routing.module';
+import { ProductDetailComponent } from './product-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductDetailComponent
+  ],
   imports: [
     CommonModule,
     ProductDetailRoutingModule
