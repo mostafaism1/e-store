1adbe4531fd0e010addfa42b308b997e2ab553c0
mostafa <me.cssign@gmail.com>
2021-05-31 16:29:21 +0200

    generate list-orders component

--- client/src/app/account/account.module.ts
+++ client/src/app/account/account.module.ts
@@ -5,13 +5,15 @@ import { AccountRoutingModule } from './account-routing.module';
 import { AccountComponent } from './account.component';
 import { AddressComponent } from './address/address.component';
 import { HelpComponent } from './help/help.component';
+import { ListOrdersComponent } from './list-orders/list-orders.component';
 
 
 @NgModule({
   declarations: [
     AccountComponent,
     AddressComponent,
-    HelpComponent
+    HelpComponent,
+    ListOrdersComponent
   ],
   imports: [
     CommonModule,
