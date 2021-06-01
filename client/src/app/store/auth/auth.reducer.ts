d708357264d92dbc132b93e328412cf512996e28
mostafa <me.cssign@gmail.com>
2021-06-01 15:23:30 +0200

    create auth reducers

--- client/src/app/store/auth/auth.reducer.ts
+++ client/src/app/store/auth/auth.reducer.ts
@@ -1,19 +1,75 @@
-import { Action, createReducer, on } from '@ngrx/store';
-
+import { createReducer, on } from '@ngrx/store';
+import { HttpError } from '..';
+import {
+  authError,
+  checkIfLoggedIn,
+  fetchVerificationStatus,
+  fetchVerificationStatusSuccess,
+  signIn,
+  signInSuccess,
+  signOut,
+  signOutSuccess,
+  signUp,
+  signUpSuccess,
+} from './auth.actions';
 
 export const authFeatureKey = 'auth';
 
-export interface State {
-
+export interface AuthState {
+  authenticated: boolean;
+  isActive: boolean;
+  errors: HttpError[];
+  loading: boolean;
 }
 
-export const initialState: State = {
-
+export const initialState: AuthState = {
+  authenticated: false,
+  isActive: false,
+  errors: [],
+  loading: false,
 };
-
 
 export const reducer = createReducer(
   initialState,
-
+  on(signUp, signIn, signOut, (authState) => {
+    return { ...authState, loading: true };
+  }),
+  on(signUpSuccess, (authState, { effect }) => {
+    return {
+      ...authState,
+      errors: authState.errors.filter((e) => e.errorEffect != effect),
+      loading: false,
+    };
+  }),
+  on(signInSuccess, (authState, { effect }) => {
+    return {
+      ...authState,
+      authenticated: true,
+      errors: authState.errors.filter((e) => e.errorEffect != effect),
+      loading: false,
+    };
+  }),
+  on(authError, (authState, { error }) => {
+    return {
+      ...authState,
+      loading: false,
+      errors: [
+        ...authState.errors.filter((e) => e.errorEffect != error.errorEffect),
+        error,
+      ],
+    };
+  }),
+  on(signOutSuccess, (authState) => initialState),
+  on(fetchVerificationStatusSuccess, (authState, { isActive }) => {
+    return {
+      ...authState,
+      isActive: isActive,
+    };
+  }),
+  on(
+    signUpSuccess,
+    checkIfLoggedIn,
+    fetchVerificationStatus,
+    (authState) => authState
+  )
 );
-
